4ce7d8e57b5bcaf9ef1eee726b071b5f367a87bc
leo <linlfx@gmail.com>
2014-12-25 23:47:20 +0800

    add path in opts. rename key to path.

--- restful-socket-client.js
+++ restful-socket-client.js
@@ -1,25 +1,28 @@
-function RestfulSocketClient(address) {
-  this.socket = io.connect(address);
+function RestfulSocketClient(address, opts) {
+  opts = opts || {};
+  opts.path = opts.path || '/restful-socket';
+
+  this.socket = io.connect(address, opts);
   this.ons = {};
 }
 
-RestfulSocketClient.prototype.proc = function (method, key, data, callback) {
-  this.socket.emit(method, {key: key, data: data});
-  if (!this.ons[method + '-' + key]) {
-    this.socket.on(method + '-' + key, callback);
-    this.ons[method + '-' + key] = 1;
+RestfulSocketClient.prototype.proc = function (method, path, data, callback) {
+  this.socket.emit(method, {path: path, data: data});
+  if (!this.ons[method + '-' + path]) {
+    this.socket.on(method + '-' + path, callback);
+    this.ons[method + '-' + path] = 1;
   }
 };
 
-RestfulSocketClient.prototype.get = function (key, data, callback) {
-  this.proc('get', key, data, callback);
+RestfulSocketClient.prototype.get = function (path, data, callback) {
+  this.proc('get', path, data, callback);
 };
-RestfulSocketClient.prototype.post = function (key, data, callback) {
-  this.proc('post', key, data, callback);
+RestfulSocketClient.prototype.post = function (path, data, callback) {
+  this.proc('post', path, data, callback);
 };
-RestfulSocketClient.prototype.put = function (key, data, callback) {
-  this.proc('put', key, data, callback);
+RestfulSocketClient.prototype.put = function (path, data, callback) {
+  this.proc('put', path, data, callback);
 };
-RestfulSocketClient.prototype.delete = function (key, data, callback) {
-  this.proc('delete', key, data, callback);
+RestfulSocketClient.prototype.delete = function (path, data, callback) {
+  this.proc('delete', path, data, callback);
 };
